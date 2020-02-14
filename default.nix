2df6af8a590107fd61b9af0e7c5b4427df1cc366
Sean Hickman <sean@grumply.com>
2020-02-13 23:38:10 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@
 }:
 mkDerivation {
   pname = "pure-websocket";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring text unordered-containers pure-lifted pure-txt pure-json mtl pure-time
